651aa65aca16b8592135e6c6ea12c4184a3bdd6c
Gabriel Gonçalves <112951586+user-GabrielSilveira@users.noreply.github.com>
2022-12-22 09:37:13 -0300

    ajustes do form

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
- function EnviarInformacoes() {
-    document.querySelector("#EnviarClick").addEventListener('EnviarInformacoes')
-        const NomeDoUsuario =  document.getElementById('#nome').value;
-        const SuaEmpresa = document.getElementById("#suaempresa").value;
-        const EmailDoUsuario =  document.getElementById('#email').value;
-        const NumeroDoUsuario = document.getElementById('#numero').value;
-        const SobreProjeto = document.getElementById('#projeto').value;
 
-        document.getElementById("BOTAO01").style.display = window.location.href =
+        const btn = document.getElementById("BOTAO01");
+        btn.addEventListener('click', function() {
+       
+        const NomeDoUsuario =  document.getElementById("nome").value;
+        const SuaEmpresa = document.getElementById("suaempresa").value;
+        const EmailDoUsuario =  document.getElementById("email").value;
+        const NumeroDoUsuario = document.getElementById("telefone").value;
+        const SobreProjeto = document.getElementById("projeto").value;
+
+         btn.style.display = window.location.href =
          window.location.href = "https://wa.me/558587261031?text=" 
          + "Nome : "+NomeDoUsuario+"%0a"
          + "Sua empresa : "+SuaEmpresa+"%0a"
          + "Email : "+EmailDoUsuario+"%0a"
          + "Número : "+NumeroDoUsuario+"%0a"
-         + "Investimento : "+investimento+"%0a"
-         + "Projeto : "+SobreProjeto+"%0a"
-         
-    
-   
-}
+         + "Projeto : "+SobreProjeto+"%0a"   
+   });
+
+
+       
+
+
